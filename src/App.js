bde27457435701d78bf3ea420129543dae2228cb
Myeong Je <dlaudwp4@gmail.com>
2019-03-10 22:57:03 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+      
+          <img src={logo}  alt="logo" />
+      <h2>Let's a develop management system!</h2>
+         
+        
+      </div>
+    );
+  }
+}
+
+export default App;
