ea6ffe3d07154951e01197372f1ba1767222cf2d
mynamemj <dlaudwp4@gmail.com>
2019-03-12 02:00:28 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -21,28 +21,24 @@ const styles = theme => ({
   }
 })
 
-const customers=[
-  {
-  'id':1,
-  'image':'https://placeimg.com/64/64/any',
-  'name':'명제',
-  'birthday':'940707',
-  'gender':'남자',
-  'job':'대학생'
 
-},
-{
-  'id':2,
-  'image':'https://placeimg.com/64/64/any',
-  'name':'초림',
-  'birthday':'970412',
-  'gender':'여자',
-  'job':'간호사'
 
-},
-]
+
 class App extends Component {
-   
+  
+  state ={ 
+    customers:""
+  }
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+  callApi= async ()=>{
+    const response= await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
     const {classes} = this.props
     return (
@@ -59,7 +55,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c=>{return( <Customer key={c.id } id ={c.id} image={c.image}name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})} 
+          {this.state.customers ?
+          this.state.customers.map(c=>{return( <Customer key={c.id } id ={c.id} image={c.image}name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);}):""} 
           </TableBody>
         </Table>
       </Paper>  
--- server.js
+++ server.js
@@ -11,8 +11,27 @@ app.get('/api/hello',(req,res)=> {
     res.send({message : 'JHello Express!'});
 });
 
-app.get('ap/hel',(req,res)=>{
-    res.send({message:"ffff"});    
+app.get('/api/customers',(req,res)=>{
+    res.send(
+        [{
+        'id':1,
+        'image':'https://placeimg.com/64/64/any',
+        'name':'명제',
+        'birthday':'940707',
+        'gender':'남자',
+        'job':'대학생'
+      
+      },
+      {
+        'id':2,
+        'image':'https://placeimg.com/64/64/any',
+        'name':'초림',
+        'birthday':'970412',
+        'gender':'여자',
+        'job':'간호사'
+      
+      }]
+      );
 });
 
 app.listen(port,()=>{console.log(`Listening on port ${port}`);});
